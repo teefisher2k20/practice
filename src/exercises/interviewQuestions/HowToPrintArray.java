5a6dadd30f36ff1f610ec36bd23de4bdc32994f2
teefisher2k20 <teefisher314@gmail.com>
2021-05-22 11:00:40 -0500

    practice for live coding

--- src/exercises/interviewQuestions/HowToPrintArray.java
+++ src/exercises/interviewQuestions/HowToPrintArray.java
@@ -7,14 +7,19 @@ public class HowToPrintArray {
         String[][] array = new String[][]{{"Element1", "Element2", "Element3"}, {"Numbers1", "Numbers2"}};
         //#1 asList() command for one dimensional arrays that you want to print.
         System.out.println(Arrays.asList(array));
+    }
 
-        //Multidimensional arrays? Use a nested for-loop using i & j
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                System.out.println(array[i][j] + "This one");
-                System.out.println(" ");
-            }
+
         }
 
-    }
-}
+
+//        //Multidimensional arrays? Use a nested for-loop using i & j
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array.length; j++) {
+//                System.out.println(array[i][j] + "This one");
+//                System.out.println(" ");
+//            }
+//        }
+
+
+
--- src/exercises/interviewQuestions/ReverseAString.java
+++ src/exercises/interviewQuestions/ReverseAString.java
@@ -5,10 +5,10 @@ public class ReverseAString {
         System.out.println(reverse("teeer"));
     }
 
-    public static String reverse(String s){
+    public static String reverse(String reverseWord){
         String output = ""; //this is the output , initialized to " "
-        for(int i=0; i<s.length(); i++){
-            output = s.charAt(i) + output; //add to String r , the character of index i
+        for(int i=0; i<reverseWord.length(); i++){
+            output = reverseWord.charAt(i) + output; //add to String r , the character of index i
         }
         return output;
     }
